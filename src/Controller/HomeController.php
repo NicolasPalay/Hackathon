59380593b6bda809be14827c9f445d0b8feff69c
Abdulkader <youssouf.soudjay@gmail.com>
2023-05-11 18:32:13 +0200

    create video background

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends AbstractController
         if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             $cityGet = array_map('trim', $_GET);
 
-            $url = "http://api.openweathermap.org/data/2.5/weather?q={$cityGet['city']}&appid=53d5eeb820d6ab866a4ff4ae1e4d9b3f&units=metric&lang=fr";
+            $url = "http://api.openweathermap.org/data/2.5/weather?q=bordeaux&appid=53d5eeb820d6ab866a4ff4ae1e4d9b3f&units=metric&lang=fr";
             $data = file_get_contents($url);
             $data = json_decode($data, true);
 
